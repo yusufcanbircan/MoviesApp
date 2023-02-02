2d07ddd753c7cb067038877cd2f309b7b1a72f98
yusufcanbircan <yusufcannnz@hotmail.com>
2023-02-02 15:00:11 +0300

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Yusuf Can Bircan on 2.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
